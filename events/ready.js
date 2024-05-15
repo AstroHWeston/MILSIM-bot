5003d60402f72d11704cbbc4fc1f988aa2e193a5
Astro <maxsimilijano@gmail.com>
2024-05-15 23:17:40 +0200

    I think I have cooked it up now :3

--- events/ready.js
+++ events/ready.js
@@ -6,11 +6,10 @@ module.exports = {
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
         //client.user.setAvatar('https://cdn.discordapp.com/attachments/1215312117792505868/1215320301248708608/flustered-cute-furry-femboy-uwu-owo.gif?ex=65fc523b&is=65e9dd3b&hm=8e334e454b700b5d796096989d1004b89a71c3a95d3c06ce89601c63b2f8e824&');
-        //client.user.setStatus('dnd');
+        client.user.setStatus('idle');
         client.user.setActivity({
             name: 'with your emotions.',
             type: ActivityType.Playing,
-            status: 'idle'
         })
     },
 };
