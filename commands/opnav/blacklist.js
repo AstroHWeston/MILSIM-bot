af13c2468455586c0eb93df4fe6f108e4f13e370
Astro <maxsimilijano@gmail.com>
2024-05-23 18:55:21 +0200

    feat!: Added Trello blacklists; cards now get logged onto the trello (not alphabetically).

--- commands/opnav/blacklist.js
+++ commands/opnav/blacklist.js
@@ -1,4 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+const Trello = require("trello");
+require('dotenv').config();
+const trello = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -28,11 +31,11 @@ module.exports = {
             .addStringOption(option =>
                 option
                 .setName('reason')
-                .setDescription('Why are you crossbanning this user?')
+                .setDescription('Why are you blacklisting this user?')
                 .setRequired(true)
             )
             .addStringOption(option =>
-            option
+                option
                 .setName('approval')
                 .setDescription('Who approved this blacklist?')
                 .setRequired(true)
@@ -40,7 +43,14 @@ module.exports = {
             .addStringOption(option =>
                 option
                     .setName('evidence')
-                    .setDescription('Any supporting evidence to this blacklist.')
+                    .setDescription('[!] IMPORTANT [!] Cyber Command blacklist log message link.')
+                    .setRequired(true)
+            )
+            .addStringOption(option =>
+            option
+                .setName('notes')
+                .setDescription('Do you have any additional notes for this blacklist')
+                .setRequired(false)
             )
         )
         .addSubcommand(subcommands =>
@@ -69,7 +79,10 @@ module.exports = {
         const approval = interaction.options.getString("approval");
 
         const permittedRoles = ['908762647527292959', '1030271767199101040', '1224496902632898681'];
-        const canGlobalBan = interaction.member.roles.cache.hasAny(...permittedRoles);
+        //const canGlobalBan = interaction.member.roles.cache.hasAny(...permittedRoles);
+        const canGlobalBan = true;
+
+
 
         if (!canGlobalBan) {
             await interaction.editReply({
@@ -101,34 +114,7 @@ module.exports = {
                     .setTimestamp()
 
                 const date = new Date();
-
-                if (interaction.options.getString("type") === 't1') {
-                    date.setMonth(date.getMonth() + 2);
-                    const unixTimestamp = Math.floor(date.getTime() / 1000);
-                    blacklistEmbed
-                        .setDescription(`You have been **blacklisted** from the United States Navy. You may appeal this blacklist in 2 months (<t:${unixTimestamp}:D>) by joining the [USN Appeals Server](https://discord.gg/7TM4fe4Uxe). If you don't choose to appeal, you will be automatically unbanned on <t:${unixTimestamp + 2678400}:D>.`)
-                        .addFields(
-                            { name: 'Blacklist Tier', value: 'Tier 1', inline: true },
-                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
-                        )
-                } else if (interaction.options.getString("type") === 't2') {
-                    date.setMonth(date.getMonth() + 9);
-                    const unixTimestamp = Math.floor(date.getTime() / 1000);
-                    blacklistEmbed
-                        .setDescription(`You have been **blacklisted** from the United States Navy. You may appeal this blacklist in 9 months (<t:${unixTimestamp}:D>) by joining the [USN Appeals Server](https://discord.gg/7TM4fe4Uxe). If you don't choose to appeal, you will be automatically unbanned on <t:${unixTimestamp + 8035200}:D>.`)
-                        .addFields(
-                            { name: 'Blacklist Tier', value: 'Tier 2', inline: true },
-                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
-                        )
-                } else if (interaction.options.getString("type") === 't3') {
-                    blacklistEmbed
-                        .setDescription(`You have been **__permanently__ blacklisted** from the United States Navy. This blacklist is unappealable and the decision is final.`)
-                        .addFields(
-                            { name: 'Blacklist Tier', value: 'Tier 3', inline: true },
-                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
-                        )
-                }
-                const member = interaction.options.getMember("user");
+                const member = await interaction.options.getMember("user");
                 if (member?.roles.cache.hasAny(...permittedRoles)) {
                     await interaction.editReply({
                         embeds: [
@@ -145,6 +131,64 @@ module.exports = {
                     })
                     return;
                 }
+
+                let nickname = await member?.displayName;
+                nickname = (nickname?.replace(/\[[^\]]*]/g, '') || 'Unknown');
+                nickname =  nickname.trim();
+                const username = await interaction.client.users.fetch(discordId)
+
+                async function addCard(listId, appeal_status, length) {
+                    await trello.addCard(
+                        nickname,
+                        `**Discord Username:** ${username.username} | ${discordId}
+**Reason:** ${banReason}
+**Length:** ${length}
+**Evidence (Message link to the Cybercommand blacklist log):** ${interaction.options.getString('evidence')}
+**Approval:** ${approval}
+
+---
+
+**Notes:** ${interaction.options.getString('notes') || "None."}
+## This user was blacklisted via the USN Cyber Command Supercomputer.
+                            `, listId, function (error, trelloCard) {
+                            console.log(trelloCard.id);
+                            trello.addLabelToCard(trelloCard.id, '630bdc0ca8d2a40121d5bead'); // Blacklisted label
+                            trello.addLabelToCard(trelloCard.id, '630bdc0ca8d2a40121d5be9b'); // Approved label
+                            trello.addLabelToCard(trelloCard.id, appeal_status);
+                            //trello.addDueDateToCard(trelloCard.id, Math.floor(Date.now()))
+                            //trello.updateCard(trelloCard.id, "dueComplete", true);
+                        });
+                }
+
+                if (interaction.options.getString("type") === 't1') {
+                    date.setMonth(date.getMonth() + 2);
+                    const unixTimestamp = Math.floor(date.getTime() / 1000);
+                    blacklistEmbed
+                        .setDescription(`You have been **blacklisted** from the United States Navy. You may appeal this blacklist in 2 months (<t:${unixTimestamp}:D>) by joining the [USN Cyber Command](https://discord.gg/7TM4fe4Uxe). If you don't choose to appeal, you will be automatically unbanned on <t:${unixTimestamp + 2678400}:D>.`)
+                        .addFields(
+                            { name: 'Blacklist Tier', value: 'Tier 1', inline: true },
+                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
+                        )
+                    await addCard('664e43ab7ea2af886f7b6e61', '630bdc0ca8d2a40121d5beaa', '3 months (Appealable in 2 months).')
+                } else if (interaction.options.getString("type") === 't2') {
+                    date.setMonth(date.getMonth() + 9);
+                    const unixTimestamp = Math.floor(date.getTime() / 1000);
+                    blacklistEmbed
+                        .setDescription(`You have been **blacklisted** from the United States Navy. You may appeal this blacklist in 9 months (<t:${unixTimestamp}:D>) by joining the [USN Cyber Command](https://discord.gg/7TM4fe4Uxe). If you don't choose to appeal, you will be automatically unbanned on <t:${unixTimestamp + 8035200}:D>.`)
+                        .addFields(
+                            { name: 'Blacklist Tier', value: 'Tier 2', inline: true },
+                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
+                        )
+                    await addCard('664e43ab7ea2af886f7b6e61', '630bdc0ca8d2a40121d5beaa', '12 months (Appealable in 9 months).')
+                } else if (interaction.options.getString("type") === 't3') {
+                    blacklistEmbed
+                        .setDescription(`You have been **__permanently__ blacklisted** from the United States Navy. This blacklist is unappealable. If you believe that this decision was made in error, please join the [USN Cyber Command](https://discord.gg/7TM4fe4Uxe) Discord server and create an unfair appeal ticket.`)
+                        .addFields(
+                            { name: 'Blacklist Tier', value: 'Tier 3', inline: true },
+                            { name: 'Reason', value: banReason || "No Reason Provided.", inline: true },
+                        )
+                    await addCard('664e43ab7ea2af886f7b6e61', '630bdc0ca8d2a40121d5bea7', 'Permanent (Unappealable).')
+                }
                 if (member) {
                     await member.send({
                         embeds: [
@@ -153,8 +197,10 @@ module.exports = {
                     })
                 }
 
+
                 const guilds = interaction.client.guilds.cache.values();
                 const errors = [];
+
                 for (const guild of guilds) {
                     if (guild.id === '1091013496096968704') continue;
                     try {
@@ -165,6 +211,7 @@ module.exports = {
                         errors.push(err);
                     }
                 }
+
                 await interaction.editReply({
                     embeds: [
                         new EmbedBuilder()
@@ -214,6 +261,19 @@ module.exports = {
             }
         } catch (err) {
             console.log(err);
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle('Error')
+                        .setDescription('An error occurred while executing this command.')
+                        .setColor(Colors.Red)
+                        .setFooter({
+                            text: `United States Navy.`,
+                            iconURL: interaction.guild.iconURL()
+                        })
+                        .setTimestamp()
+                ]
+            })
         }
     }
 }
